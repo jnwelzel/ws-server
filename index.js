73a833d9472c7201fc69df0c59a7b4351059dabd
Jonathan Welzel <jonathan.welzel@fluig.com>
2015-03-24 11:58:50 -0300

    finishing up

---
+++ index.js
@@ -0,0 +1,28 @@
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + "/"));
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    wss.broadcast(message);
+  });
+  ws.send('something');
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
